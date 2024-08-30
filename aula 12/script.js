5c86b9d3b42dce037599549c5959a3dca8642965
Miguel Pereira <miguel.p.oliveira7@aluno.senai.br>
2024-08-30 16:19:01 -0300

    odeio minha vida

---
+++ aula 12/script.js
@@ -0,0 +1,32 @@
+// let titulo = document.getElementsByTagName("h1")
+// let para = document.getElementsByClassName("para")
+// let grafo = document.getElementById("grafo")
+
+// console.log(titulo[0].textContent)
+// console.log(para[0].textContent)
+// console.log(grafo.textContent)
+
+// para[0].innerText = "paragrafou"
+
+// console.log(para[0].textContent) 
+// let clique = document.getElementById("clique")
+
+// function cliquei(){
+//     clique.style.color = "red";
+// }
+// function outroFoco(){
+//     console.log("clicou fora da caixa, mudou o foco")
+// }
+// function trocaTexto(text){
+//     let texto = document.getElementById("textoParaTrocar")
+//     texto.innerText = text.value
+// }
+function corVerde(elemento){
+    elemento.style.color = 'green'
+    elemento.style.fontSize = '30px'
+}
+
+function corAzul(elemento){
+    elemento.style.color = 'blue'
+    elemento.style.fontSize = '60px'
+}
